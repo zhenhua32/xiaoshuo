235d8c0bd59b41b7a0bf0187293264c9eb0e1de3
zhenhua32 <zhenhua32@gmail.com>
2016-07-04 15:39:15 +0800

    updates

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/cheerio/index.d.ts" />
+/// <reference path="globals/mongoose/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/request/index.d.ts" />
+/// <reference path="globals/restify/index.d.ts" />
