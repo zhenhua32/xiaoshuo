8e2e37abc26e7ccaf28c7f34e7970e3d1c6968b9
zhenhua32 <zhenhua32@gmail.com>
2016-08-04 10:57:35 +0800

    updates

--- uibootstrap/public/js/main.js
+++ uibootstrap/public/js/main.js
@@ -1,9 +1,20 @@
 // var $ = require('jquery');
 $(document).ready(function () {
   // add novel list
-  loadNovel();
-  loadChapter();
-
+  switch (window.location.pathname) {
+    case '/':
+      loadNovel();
+      break;
+    case '/novel.html':
+      loadChapter();
+      break;
+    case '/chapter.html':
+      loadContent();
+      break;
+    default:
+      loadNovel();
+      break;
+  }
 })
 
 function loadNovel() {
@@ -32,11 +43,11 @@ function loadChapter() {
 
   $.get('http://localhost:8008/novel/id/' + id)
     .then(function (data) {
-      $('#novel').append(data.title);
+      $('#novel').text(data.title);
     })
 
   $.ajax({
-    url: 'http://localhost:8008/chapter/all?novelid=' + id,
+    url: 'http://localhost:8008/chapter/all?limit=9999&skip=0&novelid=' + id,
     dataType: 'json',
     method: 'GET',
     crossDomain: true
@@ -44,7 +55,11 @@ function loadChapter() {
     .then(function (data, textStatus, jqXHR) {
       data.map(function (node) {
         $('#chapterlist').append(
-          '<a href="/novel.html?id=' + node._id + '" class="list-group-item">' + node.title + '</a>'
+          '<a href="/chapter.html?novelid=' +
+          id + '&index=' + node.index +
+          '" class="list-group-item">' +
+          node.title +
+          '</a>'
         )
       })
 
@@ -55,3 +70,51 @@ function loadChapter() {
     })
 
 }
+
+function loadContent() {
+  var query = parseQuery(window.location.search);
+
+  var base = '/chapter.html?novelid=' + query.novelid +'&index=';
+
+  if (query.index == 1) {
+    $('.pager .previous').addClass('disabled');
+  } else {
+    $('.pager .previous a').attr('href', base + (Number(query.index) - 1))
+  }
+
+  $('.pager .next a').attr('href', base + (Number(query.index) + 1))
+
+
+  $.ajax({
+    url: 'http://localhost:8008/chapter/find?novelid=' + query.novelid + '&index=' + query.index,
+    dataType: 'json',
+    method: 'GET',
+    crossDomain: true
+  })
+    .then(function (data, textStatus, jqXHR) {
+      $('#chapter-title').text(data.title);
+      data.body.split('\r\n').map(function (node) {
+        $('#chapter-body').append('<p>' + node + '</p>')
+      })
+
+    }, function (jqXHR, textStatus, err) {
+      console.log(jqXHR.responseJSON.msg);
+      console.log(textStatus);
+      console.log(err);
+    })
+
+
+
+}
+// >novelid=&index
+function parseQuery(search) {
+  var hashes = {};
+  var hash = null;
+  var query = search.slice(1).split('&');
+  for (var i = 0; i < query.length; i++) {
+    hash = query[i].split('=');
+    hashes[hash[0]] = hash[1];
+  }
+  return hashes;
+}
+
